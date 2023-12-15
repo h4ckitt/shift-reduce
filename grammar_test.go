bd0d55d1427e1afd9b01b16d8cfced283ed85c6c
h4ckitt <dammysteve10@gmail.com>
2023-12-15 18:42:54 +0100

    [add] Added Tests

--- grammar_test.go
+++ grammar_test.go
@@ -1,1 +1,28 @@
 package main
+
+import "testing"
+
+func TestGrammar_Load(t *testing.T) {
+	g := newGrammar()
+	g.Load("a", "b|c|e+j", "d", "e|f")
+
+	if g.get("b") != "a" {
+		t.Error("Expected a, got", g.get("b"))
+	}
+
+	if g.get("c") != "a" {
+		t.Error("Expected a, got", g.get("c"))
+	}
+
+	if g.get("e+j") != "a" {
+		t.Error("Expected a, got", g.get("e+j"))
+	}
+
+	if g.get("e") != "d" {
+		t.Error("Expected d, got", g.get("e"))
+	}
+
+	if g.get("f") != "d" {
+		t.Error("Expected d, got", g.get("f"))
+	}
+}
--- stack_test.go
+++ stack_test.go
@@ -1,1 +1,48 @@
 package main
+
+import "testing"
+
+func TestStack_Peek(t *testing.T) {
+	s := new(stack)
+	s.Push("a")
+	s.Push("b")
+	s.Push("c")
+
+	if s.Peek() != "c" {
+		t.Error("Expected c, got", s.Peek())
+	}
+}
+
+func TestStack_Pop(t *testing.T) {
+	s := new(stack)
+	s.Push("a")
+	s.Push("b")
+	s.Push("c")
+
+	if s.Pop() != "c" {
+		t.Error("Expected c, got", s.Pop())
+	}
+
+	if s.Pop() != "b" {
+		t.Error("Expected b, got", s.Pop())
+	}
+
+	if s.Pop() != "a" {
+		t.Error("Expected a, got", s.Pop())
+	}
+
+	if s.Pop() != "" {
+		t.Error("Expected empty string, got", s.Pop())
+	}
+}
+
+func TestStack_PeekEntireStack(t *testing.T) {
+	s := new(stack)
+	s.Push("a")
+	s.Push("b")
+	s.Push("c")
+
+	if s.PeekEntireStack() != "abc" {
+		t.Error("Expected abc, got", s.PeekEntireStack())
+	}
+}
